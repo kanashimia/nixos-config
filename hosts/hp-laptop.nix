710fcf0e4405c44da57a7e296c2fe9698820267f
Kanashimia <chad@redpilled.dev>
2020-11-22 23:51:27 +0000

    Rip hp.

--- hosts/hp-laptop.nix
+++ hosts/hp-laptop.nix
@@ -24,8 +24,11 @@
   # and, if it isn't there (because probably it isn't),
   # you have to do a hard shutdown to make it appear.
   # Idk how to feel about this, sounds like a sitcom.
+  #
+  # Edit: Ah fuck, even this doesen't work anymore.
+  # I'll keep it for historical reference.
   systemd.services.fuck-hp = {
-    enable = true;
+    enable = false;
     description = "Fuck HP";
     script = ". /dev/input/js0";
     wantedBy = [ "default.target" ];
