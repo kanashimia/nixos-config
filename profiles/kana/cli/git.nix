e6ed9c6d1c7ea967052e089c42119f4574e52e68
Kanashimia <chad@redpilled.dev>
2020-12-09 20:59:15 +0200

    Add credential helper for git.

--- profiles/kana/cli/git.nix
+++ profiles/kana/cli/git.nix
@@ -1,7 +1,14 @@
+{ pkgs, ...}:
+
 {
   programs.git = {
     enable = true;
+    package = pkgs.gitAndTools.gitFull;
     userEmail = "nikita20001116@gmail.com";
     userName = "Nikita Ursol";
+    extraConfig = {
+      credential.helper = "libsecret";
+    };
   };
+  services.gnome-keyring.enable = true;
 }
