bf474651d52fe4c44753180038e2cfbce07e3e44
Kanashimia <chad@redpilled.dev>
2021-12-02 02:06:47 +0200

    Simplify zsh prompt.

--- modules/apps/shells/zsh/prompt.sh
+++ modules/apps/shells/zsh/prompt.sh
@@ -1,23 +1,10 @@
-precmd() {
-    # local head
-    # local path="$PWD"
-    # while [[ "$path" != "" && ! -e "$path"/.git/HEAD ]]; do
-    #     path="${path%/*}"
-    # done
-    # if [[ "$path" != "" ]]; then
-    #   read head < "$path"/.git/HEAD
-    #   head="${head##ref: refs/heads/}"
-    # fi
-
-    prompt_top_line=(
-        '%F{cyan}%~%f'
-        $'%(2L.%F{red}lvl:%L%f.\e[D)'
-        # "%F{green}${head:+branch:$head}%f"
-    )
-    prompt_lines=(
-        ''
-        "${(j: :)prompt_top_line}"
-        "%F{%(?.green.red)}%(!.!.›)%f"
-    )
-    PS1="${(F)prompt_lines}"
-}
+prompt_top_line=(
+    '%F{cyan}%~%f'
+    $'%(2L.%F{red}lvl:%L%f.\e[D)'
+)
+prompt_lines=(
+    ''
+    "${(j: :)prompt_top_line}"
+    "%F{%(?.green.red)}%(!.!.›)%f"
+)
+prompt="${(F)prompt_lines}"
