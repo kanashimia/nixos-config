2250324be6f955a564d57626e62dc6256a6f1e27
Mia Kanashi <chad@redpilled.dev>
2024-05-04 09:17:10 +0300

    Terraria service insane hardening.

--- machines/personal-server/terraria.nix
+++ machines/personal-server/terraria.nix
@@ -1,6 +1,7 @@
 { pkgs, config, lib, ... }: let
-  terraria-port = 7777;
-  terraria-config = (pkgs.formats.keyValue {}).generate "terraria-config.txt" {
+  terrariaPort = 7777;
+  terrariaFifo = "/run/terraria/terraria.sock";
+  terrariaConfig = (pkgs.formats.keyValue {}).generate "terraria-config.txt" {
     world = "/var/lib/terraria/worlds/absolutely-new-world.wld";
     worldpath = "/var/lib/terraria/worlds";
     # Creates a new world if none is found. World size is specified by: 1(small), 2(medium), and 3(large).
@@ -12,8 +13,7 @@
     # Sets the difficulty of the world when using autocreate 0(classic), 1(expert), 2(master), 3(journey)
     difficulty = 2;
     maxplayers = 20;
-    port = terraria-port;
-    password = "bunker";
+    port = terrariaPort;
     motd = "Welcome to the absolutely new world! Now with extra content!";
     secure = 0;
     language = "en-US";
@@ -22,55 +22,78 @@
     npcstream = 1;
   };
 in {
-  networking.firewall = {
-    allowedTCPPorts = [ terraria-port ];
-    allowedUDPPorts = [ terraria-port ];
-  };
+  networking.firewall.allowedTCPPorts = [ terrariaPort ];
 
   nixpkgs.config.allowUnfree = true;
-
-  users.users.terraria = {
-    group = "terraria";
-    home = "/var/lib/terraria";
-    uid = config.ids.uids.terraria;
-  };
-
-  users.groups.terraria = {
-    gid = config.ids.gids.terraria;
-  };
 
   systemd.sockets.terraria = {
     partOf = [ "terraria.service" ];
     socketConfig = {
-     ListenFIFO = [ "/run/terraria.sock" ];
-     SocketUser = "terraria";
-     SocketMode = "0660";
-     RemoveOnStop = true;
+      ListenFIFO = terrariaFifo;
+      SocketMode = "0660";
+      RemoveOnStop = true;
     };
   };
 
   systemd.services.terraria = {
+    enable = true;
     wantedBy = [ "multi-user.target" ];
     after = [ "network.target" ];
     bindsTo = [ "terraria.socket" ];
 
     preStop = ''
-      printf '\nexit\n' > /run/terraria.sock
+      [ -p ${terrariaFifo} ] && printf '\nexit\n' > ${terrariaFifo}
+    '';
+
+    script = ''
+      exec ${lib.getExe pkgs.terraria-server} -config <(
+        cat ${terrariaConfig}
+        printf '\npassword='
+        cat /run/credentials/terraria.service/terrarion
+      )
     '';
 
     serviceConfig = {
-     User = "terraria";
-     ExecStart = "${lib.getExe pkgs.terraria-server} -config ${terraria-config}";
+      LoadCredentialEncrypted = "terrarion:${./secrets/terrarion.creds}";
 
-     StateDirectory = "terraria";
-     StateDirectoryMode = "0750";
+      User = "terraria";
+      DynamicUser = true;
 
-     StandardInput = "socket";
-     StandardOutput = "journal";
-     StandardError = "journal";
+      RuntimeDirectory = "terraria";
+      StateDirectory = "terraria";
+      StateDirectoryMode = "0750";
 
-     KillSignal = "SIGCONT";
-     TimeoutStopSec = "1h";
+      StandardInput = "socket";
+      StandardOutput = "journal";
+      StandardError = "journal";
+
+      SyslogIdentifier = "terraria";
+
+      KillSignal = "SIGCONT";
+      TimeoutStopSec = "1h";
+
+      # Hardening
+      CapabilityBoundingSet = "";
+      LockPersonality = true;
+      PrivateDevices = true;
+      ProcSubset = "pid";
+      PrivateTmp = true;
+      ProtectClock = true;
+      ProtectControlGroups = true;
+      ProtectHome = true;
+      ProtectHostname = true;
+      ProtectKernelLogs = true;
+      ProtectKernelModules = true;
+      ProtectKernelTunables = true;
+      ProtectProc = "invisible";
+      ProtectSystem = "strict";
+      RestrictAddressFamilies = [ "AF_INET" "AF_INET6" "AF_NETLINK" ];
+      RestrictNamespaces = true;
+      RestrictRealtime = true;
+      RestrictSUIDSGID = true;
+      SystemCallArchitectures = "native";
+      SystemCallFilter = [ "@system-service" "~@privileged" ];
+      UMask = "0077";
     };
   };
 }
