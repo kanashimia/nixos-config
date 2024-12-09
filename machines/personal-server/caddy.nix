2b634782949d3296a862bd72de6a434e08ccba9f
Mia Kanashi <chad@redpilled.dev>
2024-12-09 09:44:10 +0200

    Caddy changes.

--- machines/personal-server/caddy.nix
+++ machines/personal-server/caddy.nix
@@ -6,13 +6,13 @@
         { src = "github.com/mholt/caddy-events-exec"; rev = "055bfd2e8b8247533c7a710e11301b7d1645c933"; }
         { src = "github.com/caddy-dns/cloudflare"; rev = "44030f9306f4815aceed3b042c7f3d2c2b110c97"; }
       ];
-      caddyRev = "8e0d3e1ec56cd349f02c9d201234c56373688ddd";
-      vendorHash = "sha256-cB7YZNqpNiW3KZIIaCNmF6muoypCPEDAn8EB3BmZ22M=";
+      caddyRev = "eaaa2e5872ef9e845a50c6aade36676c0ecfe2e2";
+      vendorHash = "sha256-zngDZtuaGcJ2Y79yPdXBYamW0ll7LiOeOTBbd7RN/NM=";
     };
     configFile = pkgs.writeText "Caddyfile" ''
       {
         email acme2@redpilled.dev
-        # auto_https prefer_wildcard
+        auto_https prefer_wildcard
         acme_dns cloudflare {file.{$CREDENTIALS_DIRECTORY}/cool-secret}
 
         log {
@@ -27,6 +27,7 @@
       mta-sts.redpilled.dev/.well-known/mta-sts.txt,
       autoconfig.redpilled.dev/.well-known/mail-v1.xml,
       autoconfig.redpilled.dev/.well-known/autoconfig/mail/config-v1.1.xml,
+      redpilled.dev/jmap/*,
       redpilled.dev/.well-known/jmap {
         reverse_proxy :8080 {
           transport http {
@@ -51,6 +52,10 @@
         file_server
         root /srv/www
         import error-page
+      }
+
+      www.redpilled.dev {
+      	redir https://redpilled.dev{uri} permanent
       }
 
       *.redpilled.dev {
